0f0285b7f8f32a8a7067ae2794621bb09fdfcf83
zainab <zainabaaaa18@gmail.com>
2025-09-18 14:34:42 -0700

    updata

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 
 
@@ -15,11 +15,10 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
   
 createRoot(document.getElementById('root')).render(
    <ClerkProvider publishableKey={PUBLISHABLE_KEY}afterSignOutUrl={"/"}>
-<HashRouter>
-
-    <App />
-    </HashRouter>
+  <BrowserRouter>
   
+    <App />
+  </BrowserRouter>
    </ClerkProvider>
  
 )
