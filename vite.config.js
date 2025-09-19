3a65484bfe2370e569cbed52bc3f5e0a66ffd275
ZAINAB <132111320+zanwbia@users.noreply.github.com>
2025-09-19 04:08:35 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/project1/',
+  base:'/project1/#/',
   plugins: [
      tailwindcss(),
     react()
